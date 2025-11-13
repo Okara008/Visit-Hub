636cd49b69e2be3f6b231b9577c40852e0610b1c
Okara008 <smileotito123@gmail.com>
2025-11-13 15:45:15 +0100

    finish it 🙈🙈🙈

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
-  // base: '/Visit-Hub/',  // 👈 Add this line
-});
+  base: '/Visit-Hub/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
+})
