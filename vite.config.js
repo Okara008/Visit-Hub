bc0989df614bf1534343524bcac1dfae89893446
Okara008 <smileotito123@gmail.com>
2025-11-13 18:11:22 +0100

    Redo

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/Visit-Hub/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+  plugins: [react()],
 })
